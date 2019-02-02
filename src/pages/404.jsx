d57d21d6d98f617300298be4bfaca95ffebbc16c
Eleanor Mollett <eleanor.mollett@gmail.com>
2019-02-02 17:34:21 +0000

    fixed issue with linkedin link not showing - note, if you change something in the config, you have to restart the development server BEFORE adding it to the graphql query on the other pages, otherwise it just keeps giving you errors

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -15,7 +15,7 @@ class NotFoundRoute extends React.Component {
                 <h1 className="page__title">NOT FOUND</h1>
                 <div className="page__body">
                   <p>
-                    You just hit a route that doesn&#39;t exist... 
+                    You just hit a route that doesn&#39;t exist...
                   </p>
                 </div>
               </div>
@@ -42,12 +42,9 @@ export const pageQuery = graphql`
         }
         author {
           name
-          email
-          telegram
           twitter
           github
-          rss
-          vk
+          linkedin
         }
       }
     }
